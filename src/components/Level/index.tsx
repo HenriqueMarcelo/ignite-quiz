56e8d81b0348ced853539bc82266b959efa7f035
Marcelo Henrique <marcelohenriquedasilvafonseca@gmail.com>
2023-07-18 14:00:29 -0300

    style: lint

--- src/components/Level/index.tsx
+++ src/components/Level/index.tsx
@@ -1,8 +1,11 @@
-import { TouchableOpacity, TouchableOpacityProps, Text } from 'react-native';
+import { TouchableOpacity, TouchableOpacityProps, Text } from 'react-native'
 
-import { THEME } from '../../styles/theme';
-import { styles } from './styles';
-import Animated, { useAnimatedStyle, useSharedValue } from 'react-native-reanimated';
+import { THEME } from '../../styles/theme'
+import { styles } from './styles'
+import Animated, {
+  useAnimatedStyle,
+  useSharedValue,
+} from 'react-native-reanimated'
 
 const TYPE_COLORS = {
   EASY: THEME.COLORS.BRAND_LIGHT,
@@ -11,38 +14,47 @@ const TYPE_COLORS = {
 }
 
 type Props = TouchableOpacityProps & {
-  title: string;
-  isChecked?: boolean;
-  type?: keyof typeof TYPE_COLORS;
+  title: string
+  isChecked?: boolean
+  type?: keyof typeof TYPE_COLORS
 }
 
-export function Level({ title, type = 'EASY', isChecked = false, ...rest }: Props) {
+export function Level({
+  title,
+  type = 'EASY',
+  isChecked = false,
+  ...rest
+}: Props) {
   const scale = useSharedValue(1)
   const animatedContainerStyle = useAnimatedStyle(() => {
     return {
-      transform: [{ scale: scale.value }]
+      transform: [{ scale: scale.value }],
     }
   })
 
-  const COLOR = TYPE_COLORS[type];
+  const COLOR = TYPE_COLORS[type]
 
   return (
     <TouchableOpacity {...rest}>
-      <Animated.View style={
-        [
+      <Animated.View
+        style={[
           styles.container,
           animatedContainerStyle,
-          { borderColor: COLOR, backgroundColor: isChecked ? COLOR : 'transparent' }
-        ]
-      }>
-        <Text style={
-          [
+          {
+            borderColor: COLOR,
+            backgroundColor: isChecked ? COLOR : 'transparent',
+          },
+        ]}
+      >
+        <Text
+          style={[
             styles.title,
-            { color: isChecked ? THEME.COLORS.GREY_100 : COLOR }
-          ]}>
+            { color: isChecked ? THEME.COLORS.GREY_100 : COLOR },
+          ]}
+        >
           {title}
         </Text>
       </Animated.View>
     </TouchableOpacity>
-  );
+  )
 }
