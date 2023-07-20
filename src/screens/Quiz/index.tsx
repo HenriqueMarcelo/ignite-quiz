226f5e95f6363daf8afba33d45a1671224a8535e
Marcelo Henrique <marcelohenriquedasilvafonseca@gmail.com>
2023-07-20 12:18:27 -0300

    feat: show animation on progress bar

--- src/screens/Quiz/index.tsx
+++ src/screens/Quiz/index.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react'
-import { Alert, ScrollView, View } from 'react-native'
+import { Alert, Text, View } from 'react-native'
 
 import { useNavigation, useRoute } from '@react-navigation/native'
 
@@ -15,12 +15,16 @@ import { ConfirmButton } from '../../components/ConfirmButton'
 import { OutlineButton } from '../../components/OutlineButton'
 import Animated, {
   Easing,
+  Extrapolate,
   interpolate,
+  useAnimatedScrollHandler,
   useAnimatedStyle,
   useSharedValue,
   withSequence,
   withTiming,
 } from 'react-native-reanimated'
+import { ProgressBar } from '../../components/ProgressBar'
+import { THEME } from '../../styles/theme'
 
 interface Params {
   id: string
@@ -38,11 +42,18 @@ export function Quiz() {
   )
 
   const shake = useSharedValue(0)
+  const scrollY = useSharedValue(0)
 
   const { navigate } = useNavigation()
 
   const route = useRoute()
   const { id } = route.params as Params
+
+  const scrollHandler = useAnimatedScrollHandler({
+    onScroll: (event) => {
+      scrollY.value = event.contentOffset.y
+    },
+  })
 
   function handleSkipConfirm() {
     Alert.alert('Pular', 'Deseja realmente pular a questão?', [
@@ -128,6 +139,34 @@ export function Quiz() {
     }
   })
 
+  const fixedProgressBarStyles = useAnimatedStyle(() => {
+    return {
+      position: 'absolute',
+      paddingTop: 50,
+      backgroundColor: THEME.COLORS.GREY_500,
+      width: '110%',
+      left: '-5%',
+      zIndex: 1,
+      opacity: interpolate(scrollY.value, [50, 90], [0, 1], Extrapolate.CLAMP),
+      transform: [
+        {
+          translateY: interpolate(
+            scrollY.value,
+            [50, 100],
+            [-40, 0],
+            Extrapolate.CLAMP,
+          ),
+        },
+      ],
+    }
+  })
+
+  const headerStyle = useAnimatedStyle(() => {
+    return {
+      opacity: interpolate(scrollY.value, [50, 90], [1, 0], Extrapolate.CLAMP),
+    }
+  })
+
   useEffect(() => {
     const quizSelected = QUIZ.filter((item) => item.id === id)[0]
     setQuiz(quizSelected)
@@ -146,15 +185,27 @@ export function Quiz() {
 
   return (
     <View style={styles.container}>
-      <ScrollView
+      <Animated.View style={fixedProgressBarStyles}>
+        <Text style={styles.title}>{quiz.title}</Text>
+        <ProgressBar
+          total={quiz.questions.length}
+          current={currentQuestion + 1}
+        />
+      </Animated.View>
+
+      <Animated.ScrollView
         showsVerticalScrollIndicator={false}
         contentContainerStyle={styles.question}
+        onScroll={scrollHandler}
+        scrollEventThrottle={16}
       >
-        <QuizHeader
-          title={quiz.title}
-          currentQuestion={currentQuestion + 1}
-          totalOfQuestions={quiz.questions.length}
-        />
+        <Animated.View style={[styles.header, headerStyle]}>
+          <QuizHeader
+            title={quiz.title}
+            currentQuestion={currentQuestion + 1}
+            totalOfQuestions={quiz.questions.length}
+          />
+        </Animated.View>
 
         <Animated.View style={shakeStyleAnimated}>
           <Question
@@ -169,7 +220,7 @@ export function Quiz() {
           <OutlineButton title="Parar" onPress={handleStop} />
           <ConfirmButton onPress={handleConfirm} />
         </View>
-      </ScrollView>
+      </Animated.ScrollView>
     </View>
   )
 }
