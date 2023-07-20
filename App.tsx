9f4f46b2232ae6ea404a30f4b96e40156d754cb7
Marcelo Henrique <marcelohenriquedasilvafonseca@gmail.com>
2023-07-20 10:56:36 -0300

    chore: install gesture handler

--- App.tsx
+++ App.tsx
@@ -4,6 +4,10 @@ import {
   Roboto_400Regular,
   Roboto_700Bold,
 } from '@expo-google-fonts/roboto'
+// eslint-disable-next-line import/no-duplicates
+import 'react-native-gesture-handler'
+// eslint-disable-next-line import/no-duplicates
+import { GestureHandlerRootView } from 'react-native-gesture-handler'
 
 import { Routes } from './src/routes'
 import { Loading } from './src/components/Loading'
@@ -16,13 +20,13 @@ export default function App() {
   }
 
   return (
-    <>
+    <GestureHandlerRootView style={{ flex: 1 }}>
       <StatusBar
         barStyle="light-content"
         backgroundColor="transparent"
         translucent
       />
       <Routes />
-    </>
+    </GestureHandlerRootView>
   )
 }
